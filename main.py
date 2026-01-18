f2596184035528f64b690cd872e77b219588a6ed
BelGray <belg186@yandex.ru>
2026-01-18 22:09:27 +0300

    add sessions rotation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
+from collections import deque
 from pyrogram import Client, filters, compose
 import aiomysql
+from pyrogram.errors import FloodWait, UserDeactivated, AuthKeyUnregistered
 
 from env_loader import DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME, API_ID, API_HASH
 from log_config import logger
@@ -13,22 +15,38 @@ DB_CONFIG = {
     'db': DB_NAME
 }
 
+processed_messages = deque(maxlen=1000)
+processing_lock = asyncio.Lock()
+
+
 async def get_active_sessions() -> list:
-    """Get all active user sessions from the database"""
+    """Get all active user sessions (id, session, phone) from the database"""
     res = list()
     conn = await aiomysql.connect(**DB_CONFIG)
     async with conn.cursor() as cur:
-        await cur.execute("SELECT session_string, phone FROM accounts WHERE status = 'active'")
+        await cur.execute("SELECT id, session_string, phone FROM accounts WHERE status = 'active'")
         res = await cur.fetchall()
     conn.close()
     return res
 
 
+async def revive_accounts():
+    """Revive flood-waited accounts"""
+    conn = await aiomysql.connect(**DB_CONFIG)
+    async with conn.cursor() as cur:
+            await cur.execute("""
+                UPDATE accounts 
+                SET status = 'active' 
+                WHERE status = 'flood_wait' 
+                AND last_used < (NOW() - INTERVAL 30 MINUTE)
+            """)
+            await conn.commit()
+    conn.close()
+
+
 async def get_sources() -> list:
     """Get all source channels from the database"""
-
     res = list()
-
     conn = await aiomysql.connect(**DB_CONFIG)
     async with conn.cursor() as cur:
         await cur.execute("SELECT channel_link FROM sources")
@@ -48,24 +66,65 @@ async def get_destinations() -> list:
     return res
 
 
+async def update_account_status(phone, status):
+    """Changes account status (active -> flood_wait / banned)"""
+    try:
+        conn = await aiomysql.connect(**DB_CONFIG)
+        async with conn.cursor() as cur:
+            await cur.execute(
+                "UPDATE accounts SET status = %s WHERE phone = %s",
+                (status, phone)
+            )
+            await conn.commit()
+        conn.close()
+        logger.warning(f"Account {phone} status changed to: {status}")
+    except Exception as e:
+        logger.error(f"Status update error (phone: {phone}): {e}")
+
+
+async def add_to_history(source_msg_id, account_id, status):
+    """Logs the forwarding event to the database history"""
+    try:
+        conn = await aiomysql.connect(**DB_CONFIG)
+        async with conn.cursor() as cur:
+            await cur.execute(
+                "INSERT INTO history (source_message_id, account_id, status) VALUES (%s, %s, %s)",
+                (source_msg_id, account_id, status)
+            )
+            await conn.commit()
+        conn.close()
+    except Exception as e:
+        logger.error(f"History logging error: {e}")
+
+
+async def is_already_processed(source_msg_id):
+    """Check if message ID in history"""
+    conn = await aiomysql.connect(**DB_CONFIG)
+    async with conn.cursor() as cur:
+        await cur.execute(
+            "SELECT 1 FROM history WHERE source_message_id = %s LIMIT 1",
+            (source_msg_id,)
+        )
+        result = await cur.fetchone()
+    conn.close()
+    return result is not None
+
+
 async def main():
+    await revive_accounts()
 
     sessions = await get_active_sessions()
     if not sessions:
-        logger.error("There are no active sessions in the database! Run the add_account.py script")
+        logger.error("No active sessions found in DB! Please run add_account.py")
         return
 
     sources = await get_sources()
     destinations = await get_destinations()
 
-    logger.info(f"Accounts loaded: {len(sessions)}")
-    logger.info(f"Source channels: {sources}")
-    logger.info(f"Destination chats: {destinations}")
-
+    logger.info(f"Accounts loaded: {len(sessions)} | Sources: {len(sources)} | Destinations: {len(destinations)}")
 
     clients = []
-    for session_string, phone in sessions:
-
+    for account_id, session_string, phone in sessions:
         app = Client(
             name=f"client_{phone}",
             api_id=API_ID,
@@ -73,27 +132,72 @@ async def main():
             session_string=session_string,
             in_memory=True
         )
+
+        app.phone_number = phone
+        app.account_id = account_id
         clients.append(app)
 
+    for app in clients:
         @app.on_message(filters.chat(sources))
         async def handler(client, message):
-            logger.info(f"New message in the {message.chat.username} source channel (Message ID: {message.id})")
 
-            for dest in destinations:
-                try:
+            if await is_already_processed(message.id):
+                return
 
-                    await client.forward_messages(
-                        chat_id=dest,
-                        from_chat_id=message.chat.id,
-                        message_ids=message.id
-                    )
-                    logger.info(f"--> Message was forwarded to {dest}")
-                    await asyncio.sleep(2)
-                except Exception as e:
-                    logger.error(f"Something went wrong! Failed to forward a message to the {dest} channel: {e}")
+            unique_id = (message.chat.id, message.id)
+
+            async with processing_lock:
+                if unique_id in processed_messages:
+                    return
+                processed_messages.append(unique_id)
+
+            logger.info(f"New post {message.id} detected by {client.phone_number}")
+
+            current_destinations = await get_destinations()
+
+            for dest in current_destinations:
+                sent_successfully = False
+
+                available_clients = clients.copy()
+
+                for sender in available_clients:
+                    if sent_successfully:
+                        break
+
+                    try:
+                        logger.info(f"Trying to send via {sender.phone_number}...")
+
+                        await sender.forward_messages(
+                            chat_id=dest,
+                            from_chat_id=message.chat.id,
+                            message_ids=message.id
+                        )
+
+                        logger.info(f"Successfully forwarded to {dest}")
+
+                        await add_to_history(message.id, sender.account_id, 'success')
+
+                        sent_successfully = True
+                        await asyncio.sleep(2)
+
+                    except FloodWait as e:
+                        logger.warning(f"Account {sender.phone_number} got FloodWait for {e.value}s")
+                        await update_account_status(sender.phone_number, 'flood_wait')
+
+                    except (UserDeactivated, AuthKeyUnregistered):
+                        logger.error(f"Account {sender.phone_number} is DEAD (Banned/Revoked)!")
+                        await update_account_status(sender.phone_number, 'banned')
+                        if sender in clients:
+                            clients.remove(sender)
+
+                    except Exception as e:
+                        logger.error(f"Unknown error with {sender.phone_number}: {e}")
+
+                if not sent_successfully:
+                    logger.critical(f"Failed to forward message to {dest} with all accounts!")
 
 
-    logger.info("User bot is working...")
+    logger.info("Userbot farm is running...")
     await compose(clients)
 
 
@@ -101,4 +205,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Бот остановлен.")
+        print("Bot stopped.")
